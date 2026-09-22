ed5cf75255f623909d5222b08753b6dec5e63769
TDF Hasnat <mughasnat377@gmail.com>
2026-09-22 08:36:40 +0500

    Deplyment again

--- src/app/api/webhooks/paddle/route.ts
+++ src/app/api/webhooks/paddle/route.ts
@@ -1,4 +1,6 @@
+import prisma from "@/lib/prisma"
 import { Paddle,EventName } from "@paddle/paddle-node-sdk"
+import { NextResponse } from "next/server"
 
 const paddle = new Paddle(process.env.PADDLE_API_KEY as string)
 
@@ -6,5 +8,36 @@ export async function POST(req: Request){
     const signature =  req.headers.get('paddle-signature') as string
     const rawText = await req.text()
 
-    
+    try {
+        const event = await paddle.webhooks.unmarshal(
+            rawText,
+            process.env.PADDLE_WEBHOOK_SCRET as string,
+            signature
+        )
+
+        if(event.eventType === EventName.TransactionCompleted){
+            const userId = event.data.customData?.userId as string
+
+            if(userId){
+                await prisma?.user?.update({
+                    where:{id:userId},
+                    data:{plan:'paid'}
+                })
+            }
+        }
+        if(event.eventType === EventName.TransactionCanceled){
+            const userId = event.data.customData?.userId as string
+
+            if(userId){
+                await prisma?.user?.update({
+                    where:{id:userId},
+                    data:{plan:'free'}
+                })
+            }
+        }
+        return NextResponse.json({received:true})
+    } catch (error) {
+        return NextResponse.json({error:"Invalid signature"}, {status:400})
+    }
+
 }
