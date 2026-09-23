5994644b24d2ddea0984b98ec10a2237d2a91cd8
TDF Hasnat <mughasnat377@gmail.com>
2026-09-23 08:43:12 +0500

    small changes

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import logo from "/Code/SAAS/cv-genie/public/cv-logo.png"
+import React from "react";
 
 function Logo() {
   return (
-   <img src='logo' alt="" className='w-32 h-32' />
-  )
+    <div className="relative h-12 w-44 mt-5 overflow-hidden">
+      <img
+        src="/cv-logo.png"
+        alt="CV Genie logo"
+        className="absolute left-0 top-1/2 h-44 w-44 -translate-y-1/2 object-contain"
+      />
+    </div>
+  );
 }
 
-export default Logo
+export default Logo;
